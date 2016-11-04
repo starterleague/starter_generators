d79091a3a9ccee266c777032548c8646047980fc
Raghu Betina <rbetina@gmail.com>
2016-11-04 14:50:30 -0500

    Redirect_to :back and explicit render

--- lib/generators/starter/resource/templates/controller.rb
+++ lib/generators/starter/resource/templates/controller.rb
@@ -1,39 +1,50 @@
 class <%= plural_name.camelize %>Controller < ApplicationController
   def index
     @<%= plural_name.underscore %> = <%= class_name %>.all
+
+    render("<%= plural_name.underscore %>/index.html.erb")
   end
 
   def show
     @<%= singular_name.underscore %> = <%= class_name %>.find(params[:id])
+
+    render("<%= plural_name.underscore %>/show.html.erb")
   end
 
   def new
     @<%= singular_name.underscore %> = <%= class_name %>.new
+
+    render("<%= plural_name.underscore %>/new.html.erb")
   end
 
   def create
     @<%= singular_name.underscore %> = <%= class_name %>.new
+
 <% attributes.each do |attribute| -%>
     @<%= singular_name.underscore %>.<%= attribute.name %> = params[:<%= attribute.name %>]
 <% end -%>
 
 <% if named_routes? -%>
     if @<%= singular_name.underscore %>.save
-      redirect_to <%= plural_name %>_url, :notice => "<%= singular_name.humanize %> created successfully."
+      redirect_to :back, :notice => "<%= singular_name.humanize %> created successfully."
     else
-      render 'new'
+      render("<%= plural_name.underscore %>/new.html.erb")
     end
 <% else -%>
-    if @<%= singular_name.underscore %>.save
-      redirect_to "/<%= plural_name %>", :notice => "<%= singular_name.humanize %> created successfully."
+    save_status = @<%= singular_name.underscore %>.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "<%= singular_name.humanize %> created successfully.")
     else
-      render 'new'
+      render("<%= plural_name.underscore %>/new.html.erb")
     end
 <% end -%>
   end
 
   def edit
     @<%= singular_name.underscore %> = <%= class_name %>.find(params[:id])
+
+    render("<%= plural_name.underscore %>/edit.html.erb")
   end
 
   def update
@@ -47,13 +58,15 @@ class <%= plural_name.camelize %>Controller < ApplicationController
     if @<%= singular_name.underscore %>.save
       redirect_to <%= singular_name %>_url(@<%= singular_name %>.id), :notice => "<%= singular_name.humanize %> updated successfully."
     else
-      render 'edit'
+      render("<%= plural_name.underscore %>/edit.html.erb")
     end
 <% else -%>
-    if @<%= singular_name.underscore %>.save
-      redirect_to "/<%= plural_name %>", :notice => "<%= singular_name.humanize %> updated successfully."
+    save_status = @<%= singular_name.underscore %>.save
+
+    if save_status == true
+      redirect_to(:back, :notice => "<%= singular_name.humanize %> updated successfully.")
     else
-      render 'edit'
+      render("<%= plural_name.underscore %>/edit.html.erb")
     end
 <% end -%>
   end
@@ -63,10 +76,6 @@ class <%= plural_name.camelize %>Controller < ApplicationController
 
     @<%= singular_name.underscore %>.destroy
 
-<% if named_routes? -%>
-    redirect_to <%= plural_name %>_url, :notice => "<%= singular_name.humanize %> deleted."
-<% else -%>
-    redirect_to "/<%= plural_name %>", :notice => "<%= singular_name.humanize %> deleted."
-<% end -%>
+    redirect_to(:back, :notice => "<%= singular_name.humanize %> deleted.")
   end
 end
